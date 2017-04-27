0f0287b4b73ab24a5cd8a47e6ffd653cec9783b7
Paweł Wnuk <p.wnuk@mchtr.pw.edu.pl>
2017-04-27 17:05:05 +0200

    Działająca wersja - z wyświetlaniem elementów

--- www/routes/index.js
+++ www/routes/index.js
@@ -1,9 +1,25 @@
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var db = req.db;
+    var collection = db.get('pomiary');
+    collection.find(
+        {},
+        {
+            "limit" : 5,
+            "sort": {'ts' : -1}
+        },
+        function (e, docs) {
+            res.render('datalist', {
+                title: "Lodóweczka",
+                "datalist" : docs
+            });
+        });
+
+//    res.render('index', { title: 'Lodówka', textBelow: 'Tu za chwilę pojawi się temperatura odczytana z bazy danych ....'+count });
 });
 
 module.exports = router;
